0008f3cd8715cb38f9c4fd7311e130a2d991b611
Phuc Do <phucdt@dlsinc.com>
2021-08-09 16:28:10 +0700

    v

--- services/style_transfer/neural_style/neural_style.py
+++ services/style_transfer/neural_style/neural_style.py
@@ -29,41 +29,42 @@ def check_paths(args):
 
 
 def stylize(image_data, model, content_scale = None):
-    image = io.BytesIO(image_data)
+    with torch.no_grad():
+        image = io.BytesIO(image_data)
 
-    content_image = utils.tensor_load_rgbimage(image, scale=content_scale)
-    content_image = content_image.unsqueeze(0)
-    print('has content_image =====')
+        content_image = utils.tensor_load_rgbimage(image, scale=content_scale)
+        content_image = content_image.unsqueeze(0)
+        print('has content_image =====')
 
-    is_cuda = torch.cuda.is_available()
-    print('is_cuda', is_cuda)
-    if is_cuda:
-        content_image = content_image.cuda()
-        print('loaded cuda =====')
-    
-   
-    content_image = Variable(utils.preprocess_batch(content_image), volatile=True)
-    style_model = TransformerNet()
-    
-    if is_cuda:
-        style_model.load_state_dict(torch.load(models_path[model]))
-        style_model.to(torch.device("cuda"))
-        style_model.cuda()
-    else: 
-        style_model.load_state_dict(torch.load(models_path[model], map_location="cpu"))
-    print('load model done =====')
-
-    try: 
-        output = style_model(content_image)
-    except: 
-        print("Unexpected error:", sys.exc_info()[0])
-        raise
-    print('output done =====')
+        is_cuda = torch.cuda.is_available()
+        print('is_cuda', is_cuda)
+        if is_cuda:
+            content_image = content_image.cuda()
+            print('loaded cuda =====')
         
-    img = utils.tensor_save_bgrimage(output.data[0], is_cuda)
     
-    bio = io.BytesIO()
-    img.save(bio, "PNG")
-    # print(bio.getbuffer())
-    return bio.getbuffer()
+        content_image = Variable(utils.preprocess_batch(content_image), volatile=True)
+        style_model = TransformerNet()
+        
+        if is_cuda:
+            style_model.load_state_dict(torch.load(models_path[model]))
+            style_model.to(torch.device("cuda"))
+            style_model.cuda()
+        else: 
+            style_model.load_state_dict(torch.load(models_path[model], map_location="cpu"))
+        print('load model done =====')
+
+        try: 
+            output = style_model(content_image)
+        except: 
+            print("Unexpected error:", sys.exc_info()[0])
+            raise
+        print('output done =====')
+            
+        img = utils.tensor_save_bgrimage(output.data[0], is_cuda)
+        
+        bio = io.BytesIO()
+        img.save(bio, "PNG")
+        # print(bio.getbuffer())
+        return bio.getbuffer()
 
